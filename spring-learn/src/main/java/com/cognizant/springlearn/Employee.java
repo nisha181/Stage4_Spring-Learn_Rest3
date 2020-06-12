e99438cffa5d35814549eab1e3bcf95225257820
nisha181 <62287659+nisha181@users.noreply.github.com>
2020-06-12 20:10:50 +0530

    Add files via upload

---
+++ spring-learn/src/main/java/com/cognizant/springlearn/Employee.java
@@ -0,0 +1,58 @@
+package com.cognizant.springlearn;
+
+public class Employee {
+	private String id;
+	private String name;
+	private int salary;
+	private Department department;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String id, String name, int salary, Department department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", department=" + department + "]";
+	}
+
+}
